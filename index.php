5abab9d0a28c2592da9feee1e4b9c9aaa6917fa2
tungsten <tungsten99ie@gmail.com>
2019-02-25 22:22:49 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>MHW S/D simu</title>
+    <script src="js/simu.js"></script>
+</head>
+<body>
+    <?php  ?>
+</body>
+</html>
